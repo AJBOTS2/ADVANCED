8302d0769c1f9a744f0f15dc61082cee5152b3d7
AJBOTS2 <gamerff86420@gmail.com>
2025-01-09 16:49:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://advanced-kntv.onrender.com")
+URL = environ.get("URL", "suitable-raccoon-ajay-tech-6fd57a0b.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
